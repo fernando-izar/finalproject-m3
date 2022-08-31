9567d3026b161eb42305f4d5c89053169585ec9e
Leonardo Kratz Mendes <kratzmendes@gmail.com>
2022-08-31 18:39:04 -0300

    feat: kg and password fields

--- src/pages/ExamplePage/index.tsx
+++ src/pages/ExamplePage/index.tsx
@@ -43,7 +43,62 @@ export const ExamplePage = () => {
     
       <Container>
 
-
+        <div>
+          <TextField
+            label="With normal TextField"
+            id="outlined-start-adornment"
+            sx={{ m: 1, width: '25ch' }}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">kg</InputAdornment>,
+            }}
+          />
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <OutlinedInput
+              id="outlined-adornment-weight"
+              value={values.weight}
+              onChange={handleChange('weight')}
+              endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+              aria-describedby="outlined-weight-helper-text"
+              inputProps={{
+                'aria-label': 'weight',
+              }}
+            />
+            <FormHelperText id="outlined-weight-helper-text">Weight</FormHelperText>
+          </FormControl>
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff/> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          
+          <FormControl fullWidth sx={{ m: 1 }}>
+            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={values.amount}
+              onChange={handleChange('amount')}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              label="Amount"
+            />
+          </FormControl>
+        </div>
 
         <form>
 
