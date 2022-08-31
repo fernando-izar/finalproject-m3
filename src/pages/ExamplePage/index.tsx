10dae54c1be3358160f4e3ed78a95a319a280dd1
Leonardo Kratz Mendes <kratzmendes@gmail.com>
2022-08-31 18:08:34 -0300

    fix: pull from origin updated

--- src/pages/ExamplePage/index.tsx
+++ src/pages/ExamplePage/index.tsx
@@ -1,16 +1,50 @@
-import { Button, Checkbox, FormControl, FormControlLabel, FormHelperText, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select } from "@material-ui/core";
-import { SelectChangeEvent } from "@mui/material";
+import { Button, Checkbox, FormControlLabel, FormHelperText, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select } from "@material-ui/core";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { FormControl, IconButton, InputAdornment, OutlinedInput, SelectChangeEvent, TextField } from "@mui/material";
 import { ChangeEvent, ReactNode, useState } from "react";
 
 import { Container } from "./styles";
 
+interface State {
+  amount: string;
+  password: string;
+  weight: string;
+  weightRange: string;
+  showPassword: boolean;
+}
+
 export const ExamplePage = () => {
 
+  const [values, setValues] = useState<State>({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
 
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
    
   return (
     
       <Container>
+
+
+
         <form>
 
           
