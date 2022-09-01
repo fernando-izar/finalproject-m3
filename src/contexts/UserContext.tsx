d1bb14af2f0ecd878e4d135adb2341d484c2177c
fernando-izar <fizar@me.com>
2022-09-01 08:27:28 -0300

    fix: UserContext.tsx line 56, change from response.data.token to response.data.accessToken

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 
@@ -45,13 +45,15 @@ const UserContextProvider = ({ children }: IUserContextProviderProps) => {
   const [user, setUser] = useState<IUser | null>(null);
   const navigate = useNavigate();
 
+  useEffect(() => {});
+
   const loginData = (data: ILoginDataProps) => {
     axios
       .post("https://api-m3-g2.herokuapp.com/login", data)
       .then((response) => {
         console.log(response.data);
         window.localStorage.clear();
-        window.localStorage.setItem("@userToken", response.data.token);
+        window.localStorage.setItem("@userToken", response.data.accessToken);
         window.localStorage.setItem("@userID", response.data.user.id);
         toast.success("Login efetuado com sucesso!");
         navigate("/home", { replace: true });
