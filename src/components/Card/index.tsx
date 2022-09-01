daba46f281998f6f717e40dd0272b20e66329f09
fernando-izar <fizar@me.com>
2022-09-01 06:50:29 -0300

    feat: flipcard test works

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,23 @@
-import { Container } from "./styles";
+import { Container, FlipCard } from "./styles";
+import hortifruti from "../../assets/Hortifruti1.png";
 
 export const Card = () => {
-  return <Container>Card</Container>;
+  return (
+    <Container>
+      <FlipCard>
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img src={hortifruti} alt="hortifruti" />
+          </div>
+          <div className="flip-card-back">
+            <h1>teste tag h1</h1>
+            <p>teste tag p</p>
+            <button onClick={() => window.alert("button works")}>
+              button test
+            </button>
+          </div>
+        </div>
+      </FlipCard>
+    </Container>
+  );
 };
