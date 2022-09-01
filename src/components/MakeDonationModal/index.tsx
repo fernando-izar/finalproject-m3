945c83b6b6f28fb01db5b5f86c80debe4e60af5c
fernando-izar <fizar@me.com>
2022-08-31 22:25:36 -0300

    feat: MakeDonationModal is opening

--- src/components/MakeDonationModal/index.tsx
+++ src/components/MakeDonationModal/index.tsx
@@ -1,5 +1,37 @@
+import { ReactNode, useContext, useEffect, useRef } from "react";
+import { Children } from "react";
 import { Container } from "./styles";
+import { DonationContext } from "../../contexts/DonationContext";
+import { ModelTraining } from "@mui/icons-material";
 
-export const MakeDonationModal = () => {
-  return <Container>MakeDonationModal</Container>;
+interface IMakeDonationModalProps {
+  children: ReactNode;
+}
+
+export const MakeDonationModal = ({ children }: IMakeDonationModalProps) => {
+  const { setIsMakeDonationModal } = useContext(DonationContext);
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOutClick = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (modalRef.current && !modalRef.current.contains(target)) {
+        setIsMakeDonationModal(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutClick);
+    };
+  });
+
+  return (
+    <Container>
+      <div className="modal-make-donations" ref={modalRef}>
+        {children}
+      </div>
+    </Container>
+  );
 };
