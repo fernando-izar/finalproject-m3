5a5510e8b1b02a327fea5c629e326db597fad58d
alex_wfarias <alex_wfarias@yahoo.com.br>
2022-09-02 22:21:27 -0300

    feat: mobile initial page

--- src/components/BigButtonMobile/index.tsx
+++ src/components/BigButtonMobile/index.tsx
@@ -2,18 +2,19 @@ import { styled } from "@mui/material/styles";
 import MuiButton from "@mui/material/Button";
 import "inter-ui/inter.css";
 
-export const BigButton = styled(MuiButton)((props) => ({
-  width: "450px",
-  height: "120px",
-  "margin-top": "100px",
-  "margin-left": "30vw",
+export const BigButton2 = styled(MuiButton)((props) => ({
+  width: "250px",
+  height: "90px",
+  // marginTop: "100px",
+  marginLeft: "20vw",
+  //   marginRight: "auto",
   borderRadius: "15px",
   backgroundColor: "#FA5C5C",
   color: "#FFFFFF",
   boxShadow: "5px 5px 5px 0px rgb(0, 0, 0, 0.25)",
   fontFamily: "Inter, system-ui",
-  fontSize: "40px",
-  lineHeight: "45px",
+  fontSize: "25px",
+  lineHeight: "25px",
   fontWeight: "700",
   textTransform: "none",
   "&:hover": {
--- src/pages/InitialPage/index.tsx
+++ src/pages/InitialPage/index.tsx
@@ -1,7 +1,9 @@
 import { Container } from "./styles";
 import initialPageImg from "../../assets/InitialPageDiamond.png";
 import logo from "../../assets/Alimento-solidario3.png";
-import { BigButton } from "../../components/BigButton";
+import { BigButton } from "../../components/BigButtonDesktop";
+import { BigButton2 } from "../../components/BigButtonMobile";
+
 import { UserContext } from "../../contexts/UserContext";
 import { useContext } from "react";
 
@@ -9,17 +11,19 @@ export const InitialPage = () => {
   const { toRegister } = useContext(UserContext);
   return (
     <Container>
-      <figure className="figBackground">
-        <img className="imgBackground" src={initialPageImg} />
-      </figure>
       <figure className="figLogo">
         <img className="imgLogo" src={logo} alt="" />
       </figure>
       <h1>Vamos juntos vencer a batalha contra a fome no Brasil!</h1>
-
-      <BigButton onClick={() => toRegister()} type="submit">
-        Entre você também nessa luta!
-      </BigButton>
+      {window.innerWidth > 540 ? (
+        <BigButton onClick={() => toRegister()} type="submit">
+          Entre você também nessa luta!
+        </BigButton>
+      ) : (
+        <BigButton2 onClick={() => toRegister()} type="submit">
+          Entre você também nessa luta!
+        </BigButton2>
+      )}
     </Container>
   );
 };
