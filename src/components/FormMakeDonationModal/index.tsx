1a26770551b6ccc47a3a7f7ff76bbc5583a6e943
fernando-izar <fizar@me.com>
2022-09-03 14:00:11 -0300

    fix: bug date

--- src/components/FormMakeDonationModal/index.tsx
+++ src/components/FormMakeDonationModal/index.tsx
@@ -1,12 +1,13 @@
 import { useContext, useState } from "react";
 import { DonationContext } from "../../contexts/DonationContext";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { IDonation } from "../../contexts/DonationContext";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { schemaDonation } from "../../validators/schemas";
 import { Button } from "@material-ui/core";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import dayjs, { Dayjs } from "dayjs";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -21,12 +22,15 @@ export const FormMakeDonationModal = () => {
   const {
     register,
     handleSubmit,
+    control,
     formState,
     formState: { errors },
   } = useForm<IDonation>({ resolver: yupResolver(schemaDonation) });
 
   //SELETOR DE DATA
-  const [value, setValue] = useState<string | null>(null);
+  const newDate = new Date();
+  const dateString = moment(newDate).format("DD/MM/YYYY");
+  const [value, setValue] = useState<string | null>(dateString);
 
   return (
     <Container>
@@ -58,16 +62,16 @@ export const FormMakeDonationModal = () => {
         />
         <p>{errors.expiration?.message}</p> */}
 
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
             disablePast
             label="Qual a validade do produto?"
-            value={value}
             inputFormat="DD/MM/YYYY"
             onChange={(value) => {
               setValue(value);
               console.log(value);
             }}
+            value={value}
             renderInput={(params) => (
               <TextField
                 {...params}
@@ -80,7 +84,39 @@ export const FormMakeDonationModal = () => {
             )}
           />
           <p>{errors.expiration?.message}</p>
-        </LocalizationProvider>
+        </LocalizationProvider> */}
+
+        <Controller
+          name="expiration"
+          // defaultValue={dateString}
+          control={control}
+          render={({ field: { onChange, ...restField } }) => (
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DesktopDatePicker
+                disablePast
+                label="Qual a validade do produto?"
+                inputFormat="DD/MM/YYYY"
+                onChange={(event) => {
+                  onChange(event);
+                  setValue(event);
+                }}
+                // value={value}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="standard"
+                    color="warning"
+                    type="date"
+                    InputLabelProps={{ color: "primary" }}
+                    {...register("expiration")}
+                  />
+                )}
+                {...restField}
+              />
+              <p>{errors.expiration?.message}</p>
+            </LocalizationProvider>
+          )}
+        />
 
         <label htmlFor="class">Classe</label>
         <select {...register("class")} id="class">
