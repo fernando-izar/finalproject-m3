93096f57ac990d8c93c614ba45d2dd46c4c177e7
fernando-izar <fizar@me.com>
2022-09-04 07:00:55 -0300

    feat: flip X 180 deg CartCard is working

--- src/components/CartCard/styles.ts
+++ src/components/CartCard/styles.ts
@@ -1,23 +1,45 @@
 import styled from "styled-components";
 
 export const Container = styled.li`
+  background-color: transparent;
   width: 260px;
-  min-height: 69px;
-  background: #e4e4e4;
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.25);
+  height: 69px;
+  /* box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.25); */
   border-radius: 4px;
+  perspective: 1000px;
 
-  .flip-card-inner {
+  .flipper {
     position: relative;
+    /* display: flex; */
     width: 100%;
     height: 100%;
     text-align: center;
-    transition: transform 0.6s;
+    transition: transform 0.8s;
     transform-style: preserve-3d;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   }
 
-  :hover .flip-card-inner {
-    transform: rotateX(180deg);
+  :hover .flipper {
+    transform: rotateX(180deg) /* rotateZ(180deg) */;
+  }
+
+  .front,
+  .back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+  }
+
+  .front {
+    /* background-color: #bbb; */
+    color: black;
+  }
+
+  .back {
+    background-color: #bdf1ea;
+    color: white;
+    transform: rotateX(180deg) /* rotateX(180deg) */;
   }
 `;
