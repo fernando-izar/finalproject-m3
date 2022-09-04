20824459ada6e0e5c1b77fc515ce58a11df64075
fernando-izar <fizar@me.com>
2022-09-03 22:49:06 -0300

    feat: start CartCard

--- src/components/CartCard/index.tsx
+++ src/components/CartCard/index.tsx
@@ -1,5 +1,14 @@
 import { Container } from "./styles";
 
 export const CartCard = () => {
-  return <Container>Example Cart Card</Container>;
+  return (
+    <Container>
+      <div className="flip-container">
+        <div className="flipper">
+          <div className="front"></div>
+          <div className="back"></div>
+        </div>
+      </div>
+    </Container>
+  );
 };
