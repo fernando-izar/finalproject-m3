5a0737168422d1a80497140ca153dab527c8af36
alex_wfarias <alex_wfarias@yahoo.com.br>
2022-09-04 18:46:28 -0300

    feat: mobile styled login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,53 @@
 import { useContext } from "react";
 import { ILoginDataProps, UserContext } from "../../contexts/UserContext";
-import { Container } from "./styles";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { schemaLogin } from "../../validators/schemas";
-import { Button } from "@material-ui/core";
+import { Button, InputLabel } from "@material-ui/core";
+import { AccountCircle, Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { ChangeEvent, useState } from "react";
+
+import { Container } from "./styles";
+import { ButtonLogin } from "../../components/ButtonLoginDesktop";
+import { TitlesH1 } from "../../components/Titles/styles";
+import { ButtonLoginMob } from "../../components/ButtonLoginMobile/styles";
+
+interface State {
+  password: string;
+  showPassword: boolean;
+}
 
 export const Login = () => {
+  const [values, setValues] = useState<State>({
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange =
+    (prop: keyof State) => (event: ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   const { toRegister, loginData } = useContext(UserContext);
   const {
     register,
@@ -16,43 +57,114 @@ export const Login = () => {
 
   return (
     <Container>
-      <form onSubmit={handleSubmit(loginData)}>
-        <h2>Login</h2>
+      <form className="formLogin" onSubmit={handleSubmit(loginData)}>
+        <TitlesH1>Login</TitlesH1>
 
-        <label htmlFor="email">Email</label>
+        {window.innerWidth > 540 ? (
+          <>
+            <TextField
+              sx={{ ml: 3, mt: 3, width: "50ch" }}
+              label="E-mail"
+              id="outlined-basic"
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+              {...register("email")}
+            />
+            <p>{errors.email?.message}</p>
+            <FormControl sx={{ ml: 3, width: "50ch" }} variant="outlined">
+              <InputLabel htmlFor="outlined-basic">Senha</InputLabel>
+              <OutlinedInput
+                label="Password"
+                id="outlined-basic"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                {...register("password")}
+                onChange={handleChange("password")}
+              />
+            </FormControl>
+            <p>{errors.password?.message}</p>
 
-        <input
-          type="text"
-          id="email"
-          placeholder="Digite seu email"
-          {...register("email")}
-        />
-        <p>{errors.email?.message}</p>
+            <ButtonLogin type="submit" onClick={() => handleSubmit}>
+              Entrar
+            </ButtonLogin>
 
-        <label htmlFor="password">Senha</label>
-        <input
-          type="password"
-          id="password"
-          placeholder="Digite sua senha"
-          {...register("password")}
-        />
-        <p>{errors.password?.message}</p>
+            <p className="pMsgReg">Você ainda não possui uma conta?</p>
 
-        <button type="submit">Entrar</button>
-        <p className="pMsgReg">Você ainda não possui uma conta?</p>
+            <ButtonLogin onClick={() => toRegister()} type="submit">
+              Cadastre-se
+            </ButtonLogin>
+          </>
+        ) : (
+          <>
+            <TextField
+              sx={{ ml: 3, mt: 3, width: "35ch" }}
+              label="E-mail"
+              id="outlined-basic"
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+              {...register("email")}
+            />
+            <p>{errors.email?.message}</p>
+            <FormControl sx={{ ml: 3, width: "35ch" }} variant="outlined">
+              <InputLabel htmlFor="outlined-basic">Senha</InputLabel>
+              <OutlinedInput
+                label="Password"
+                id="outlined-basic"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                {...register("password")}
+                onChange={handleChange("password")}
+              />
+            </FormControl>
+            <p>{errors.password?.message}</p>
 
-        <button onClick={() => toRegister()} type="submit">
-          Cadastre-se
-        </button>
-        <Button
-          onClick={() => toRegister()}
-          type="submit"
-          variant="contained"
-          color="primary"
-          size="large"
-        >
-          Cadastre-se 2
-        </Button>
+            <ButtonLoginMob type="submit" onClick={() => handleSubmit}>
+              Entrar
+            </ButtonLoginMob>
+
+            <p className="pMsgReg">Você ainda não possui uma conta?</p>
+
+            <ButtonLoginMob onClick={() => toRegister()} type="submit">
+              Cadastre-se
+            </ButtonLoginMob>
+          </>
+        )}
       </form>
     </Container>
   );
