f7bf127630bf5385629f7838a0e86ca57c03f114
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2022-09-05 03:51:38 -0400

    feat:add back-card charity only shows to charity type users

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { IAllDataDonation } from "../../contexts/DonorContext";
 import { FlipCard } from "./styles";
 import { DonationContext } from "../../contexts/DonationContext";
+import { UserContext } from "../../contexts/UserContext";
 
 export const Card = ({
   food,
@@ -14,7 +15,8 @@ export const Card = ({
   user,
 }: IAllDataDonation) => {
   const { chooseImg } = useContext(DonationContext);
-
+  const { user: loggedUser } = useContext(UserContext);
+  console.log(loggedUser?.name);
   return (
     <FlipCard>
       <div className="flip-card-inner">
@@ -40,35 +42,40 @@ export const Card = ({
         </div>
 
         <div className="flip-card-back">
-          {/* Atrás do card para donatário*/}
-          <div className="flip-card-back__information">
-            <div className="flip-card-back__information--header">
-              <div className="flip-card-back__information--food">
-                <p>{food}</p>
-                <span>{classification}</span>
+          {loggedUser?.type === "charity" ? (
+            <>
+              <div className="flip-card-back__information">
+                <div className="flip-card-back__information--header">
+                  <div className="flip-card-back__information--food">
+                    <p>{food}</p>
+                    <span>{classification}</span>
+                  </div>
+
+                  <div className="flip-card-back__information--quantity">
+                    <p>Quantidade</p>
+                    <span>{quantity}</span>
+                  </div>
+                </div>
+
+                <div className="flip-card-back__information--donor-data">
+                  <p>{user.name}</p>
+                  <span>{`${user.address}, ${user.complement}`}</span>
+                  <span>
+                    {user.city}/{user.state}
+                  </span>
+                  <span>{user.contact}</span>
+                </div>
+
+                <div className="flip-card-back__information--expiration-date">
+                  <p>Validade</p>
+                  <span>{expiration}</span>
+                </div>
               </div>
-
-              <div className="flip-card-back__information--quantity">
-                <p>Quantidade</p>
-                <span>{quantity}</span>
-              </div>
-            </div>
-
-            <div className="flip-card-back__information--donor-data">
-              <p>{user.name}</p>
-              <span>{`${user.address}, ${user.complement}`}</span>
-              <span>
-                {user.city}/{user.state}
-              </span>
-              <span>{user.contact}</span>
-            </div>
-
-            <div className="flip-card-back__information--expiration-date">
-              <p>Validade</p>
-              <span>{expiration}</span>
-            </div>
-          </div>
-          <button>Reservar Alimento</button>
+              <button>Reservar Alimento</button>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </FlipCard>
